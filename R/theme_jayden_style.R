a9dfe8184955bf37a932fdb6c53ef65f37786e9d
jaydenortiz <156019125+jaydenortiz@users.noreply.github.com>
2024-11-12 15:56:46 -0500

    JaydenPackage Updated

--- R/theme_jayden_style.R
+++ R/theme_jayden_style.R
@@ -1,4 +1,4 @@
-theme_jayden_style <- function(base_size = 12, base_family = "Arial") {
+theme_jayden_style <- function(base_size = 8, base_family = "Arial") {
   ggplot2::theme_minimal(base_size = base_size, base_family = base_family) +
     ggplot2::theme(
       plot.title = ggplot2::element_text(
@@ -6,32 +6,32 @@ theme_jayden_style <- function(base_size = 12, base_family = "Arial") {
         hjust = 0.5,
         color = "#4B9CD3",
         family = "Helvetica",
-        size = 18,
+        size = 12,
         vjust = 1
       ),
       axis.title = ggplot2::element_text(
         face = "bold",
         color = "#FF6347",
-        size = 16
+        size = 10
       ),
       axis.text = ggplot2::element_text(
         color = "#333333",
-        size = 14
+        size = 8
       ),
       panel.grid.major = ggplot2::element_line(
         color = "#CCCCCC",
-        linewidth = 0.7
+        linewidth = 0.5
       ),
       panel.grid.minor = ggplot2::element_blank(),
       legend.position = "bottom",
       legend.title = ggplot2::element_text(
         face = "italic",
         color = "#8B4513",
-        size = 14
+        size = 8
       ),
       legend.text = ggplot2::element_text(
         color = "#696969",
-        size = 12
+        size = 6
       )
     )
 }
